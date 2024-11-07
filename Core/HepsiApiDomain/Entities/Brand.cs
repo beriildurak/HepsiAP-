75f7442fd0dc5ee84b261416b96cc8035985b800
Beril Durak <berildurak60bd@gmail.com>
2024-11-08 02:40:48 +0300

    ders4

---
+++ Core/HepsiApiDomain/Entities/Brand.cs
@@ -0,0 +1,19 @@
+﻿using HepsiApiDomain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiApiDomain.Entities
+{
+    public class Brand:EntityBase,IEntityBase
+    {
+        public Brand() { }
+        public Brand(string name) {
+            Name = name;
+        }
+        public required string Name { get; set; }
+
+    }
+}
